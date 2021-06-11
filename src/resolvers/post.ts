4b9fdf79b4b146322e76b586ec8aa5e43b255895
Raymond Hunce <hunce18c@gmail.com>
2021-06-10 21:18:39 -0400

    Create Post queries and mutations resolvers

---
+++ src/resolvers/post.ts
@@ -0,0 +1,30 @@
+import { MyContext } from '../types';
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { Post } from '../entities/Post';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em } : MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id') id: number,
+    @Ctx() { em } : MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em } : MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post;
+  }
+
+}
