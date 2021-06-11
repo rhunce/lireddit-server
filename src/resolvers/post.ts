b04c9a626a71ccddfb55beb8cbf00d2131edbe28
Raymond Hunce <hunce18c@gmail.com>
2021-06-10 21:26:47 -0400

    Format Post Resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -5,26 +5,32 @@ import { Post } from '../entities/Post';
 @Resolver()
 export class PostResolver {
   @Query(() => [Post])
-  posts(@Ctx() { em } : MyContext): Promise<Post[]> {
+  posts(
+    @Ctx()
+    { em } : MyContext
+  ): Promise<Post[]> {
     return em.find(Post, {});
   }
 
   @Query(() => Post, { nullable: true })
   post(
-    @Arg('id') id: number,
-    @Ctx() { em } : MyContext
+    @Arg('id')
+    id: number,
+    @Ctx()
+    { em } : MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
   }
 
   @Mutation(() => Post)
   async createPost(
-    @Arg('title') title: string,
-    @Ctx() { em } : MyContext
+    @Arg('title')
+    title: string,
+    @Ctx()
+    { em } : MyContext
   ): Promise<Post> {
     const post = em.create(Post, { title })
     await em.persistAndFlush(post)
     return post;
   }
-
 }
